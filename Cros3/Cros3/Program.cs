608f697b6842c1555d2271cbbf746c84e1e14d66
Vados <78901174+Vados11@users.noreply.github.com>
2023-10-01 14:43:14 +0300

    Add files via upload

---
+++ Cros3/Cros3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Edge
+{
+    public int U { get; set; }
+    public int V { get; set; }
+    public int Weight { get; set; }
+    public int Index { get; set; }
+
+    public Edge(int u, int v, int weight, int index)
+    {
+        U = u;
+        V = v;
+        Weight = weight;
+        Index = index;
+    }
+}
+
+public class Program
+{
+    public static List<Edge>[] adjList;
+    public static bool[] visited;
+
+    public static void DFS(int node)
+    {
+        visited[node] = true;
+        foreach (var edge in adjList[node])
+        {
+            if (!visited[edge.V])
+                DFS(edge.V);
+        }
+    }
+
+    public static void Main()
+    {
+        var lines = File.ReadAllLines("INPUT.TXT");
+        var nm = lines[0].Split(' ').Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        var edges = new List<Edge>();
+
+        for (int i = 1; i <= m; i++)
+        {
+            var edgeData = lines[i].Split(' ').Select(int.Parse).ToArray();
+            edges.Add(new Edge(edgeData[0] - 1, edgeData[1] - 1, edgeData[2], i));
+        }
+
+        int bestCost = int.MaxValue;
+        List<int> bestPath = null;
+
+        for (int mask = 0; mask < (1 << m); mask++)
+        {
+            adjList = new List<Edge>[n];
+            for (int i = 0; i < n; i++)
+            {
+                adjList[i] = new List<Edge>();
+            }
+
+            int currentCost = 0;
+
+            for (int j = 0; j < m; j++)
+            {
+                if ((mask & (1 << j)) != 0)
+                {
+                    adjList[edges[j].U].Add(edges[j]);
+                    currentCost += edges[j].Weight;
+                }
+            }
+
+            visited = new bool[n];
+            DFS(0);
+
+            if (visited.All(v => v) && currentCost < bestCost)
+            {
+                bestCost = currentCost;
+                bestPath = new List<int>();
+
+                for (int j = 0; j < m; j++)
+                {
+                    if ((mask & (1 << j)) != 0)
+                        bestPath.Add(edges[j].Index);
+                }
+            }
+        }
+
+        File.WriteAllText("OUTPUT.TXT", $"{bestCost} {bestPath.Count}\n{string.Join(" ", bestPath)}");
+    }
+}
