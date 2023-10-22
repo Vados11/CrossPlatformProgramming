948867b48a25cf49dec78f49559a355667ff3609
Vados <78901174+Vados11@users.noreply.github.com>
2023-10-22 17:41:02 +0300

    Add files via upload

--- Cros2/Cros2/Program.cs
+++ Cros2/Cros2/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cros2
 {
@@ -11,37 +9,51 @@ namespace Cros2
     {
         static void Main()
         {
-            // Читаємо вхідні дані з файлу
-            string[] lines = File.ReadAllLines("INPUT.TXT");
-            int N = int.Parse(lines[0]);
-            int[] coins = lines[1].Split().Select(int.Parse).ToArray();
-            int K = int.Parse(lines[2]);
-
-            // Ініціалізуємо масив dp значеннями, що відповідають нескінченності
-            int[] dp = new int[K + 1];
-            for (int i = 1; i <= K; i++)
+            try
             {
-                dp[i] = int.MaxValue;
-            }
-
-            // Встановлюємо початкове значення dp[0]
-            dp[0] = 0;
-
-            // Обчислюємо мінімальну кількість монет для кожної можливої суми від 1 до K
-            for (int i = 1; i <= K; i++)
-            {
-                foreach (int coin in coins)
+                // Читаємо вхідні дані з файлу
+                string[] lines = File.ReadAllLines("INPUT.TXT");
+                if (lines.Length < 3)
                 {
-                    if (i - coin >= 0 && dp[i - coin] != int.MaxValue)
+                    throw new Exception("Not enough input data");
+                }
+
+                int N = int.Parse(lines[0]);
+                int[] coins = lines[1].Split().Select(int.Parse).ToArray();
+                int K = int.Parse(lines[2]);
+
+                // Ініціалізуємо масив dp значеннями, що відповідають нескінченності
+                int[] dp = new int[K + 1];
+                for (int i = 1; i <= K; i++)
+                {
+                    dp[i] = int.MaxValue;
+                }
+
+                // Встановлюємо початкове значення dp[0]
+                dp[0] = 0;
+
+                // Обчислюємо мінімальну кількість монет для кожної можливої суми від 1 до K
+                for (int i = 1; i <= K; i++)
+                {
+                    foreach (int coin in coins)
                     {
-                        dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                        if (i - coin >= 0 && dp[i - coin] != int.MaxValue)
+                        {
+                            dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                        }
                     }
                 }
-            }
 
-            // Виводимо результат у вихідний файл
-            int result = dp[K] == int.MaxValue ? -1 : dp[K];
-            File.WriteAllText("OUTPUT.TXT", result.ToString());
+                // Виводимо результат у вихідний файл
+                int result = dp[K] == int.MaxValue ? -1 : dp[K];
+                File.WriteAllText("OUTPUT.TXT", result.ToString());
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText("OUTPUT.TXT", "-1"); // Вивести -1 у випадку помилки
+                Console.WriteLine("Error: " + ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
